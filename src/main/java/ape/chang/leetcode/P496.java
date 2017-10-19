ed5c3676a0dc1ab5135165721538525f6e585e62
ape-Chang <ape.chang.1989@gmail.com>
2017-10-19 22:35:00 +0800

    a little touch on 678

--- src/main/java/ape/chang/leetcode/P496.java
+++ src/main/java/ape/chang/leetcode/P496.java
@@ -1,8 +1,5 @@
 package ape.chang.leetcode;
 
-import ape.chang.leetcode.Solution.IntIntMap;
-import ape.chang.leetcode.Solution.IntStack;
-
 public class P496 {
 	
 	class Solution {
@@ -25,6 +22,148 @@ public class P496 {
 				R[i] = next.getOrDefault(A[i], -1);
 			}
 			return R;
+		}
+		
+		class IntStack {
+			private int[] stack = new int[64];
+			private int p = -1;
+
+			public IntStack() {
+			}
+
+			public void push(int x) {
+				stack[++p] = x;
+				if (p == stack.length) {
+					// expand
+					int[] newStack = new int[stack.length * 2];
+					System.arraycopy(stack, 0, newStack, 0, stack.length);
+					stack = newStack;
+				}
+			}
+
+			public int top() {
+				// ignore bound check
+				return stack[p];
+			}
+
+			public int pop() {
+				// ignore bound check
+				return stack[p--];
+			}
+
+			public boolean empty() {
+				return p < 0;
+			}
+		}
+
+		/*
+		 * Not thread safe. Open index. seems OK
+		 */
+		class IntIntMap {
+
+			int[] keys;
+			int[] values;
+
+			int zeroValue;
+			boolean zeroValuePutted;
+
+			int mask;
+
+			int size;
+			int capacity;
+
+			public IntIntMap() {
+				capacity = 1 << 8;
+				mask = capacity - 1;
+				keys = new int[capacity];
+				values = new int[capacity];
+			}
+
+			public void put(int key, int value) {
+				if (key == 0) {
+					zeroValue = value;
+					if (zeroValuePutted) {
+						return;
+					} else {
+						zeroValuePutted = true;
+						size++;
+					}
+
+				} else {
+					int index;
+					for (int k = key;; k++) {
+						index = indexOf(k);
+						if (keys[index] == 0) {
+							break;
+						}
+
+						if (keys[index] == key) {
+							values[index] = value;
+							return;
+						}
+					}
+
+					keys[index] = key;
+					values[index] = value;
+					size++;
+				}
+
+				if (size > capacity / 2) {
+					rehash();
+				}
+			}
+
+			private void rehash() {
+				int[] keys = this.keys;
+				int[] values = this.values;
+				this.capacity *= 2;
+				this.mask = capacity - 1;
+				this.keys = new int[capacity];
+				this.values = new int[capacity];
+				for (int i = 0, n = keys.length; i < n; ++i) {
+					if (keys[i] != 0) {
+						put(keys[i], values[i]);
+					}
+				}
+			}
+
+			public int getOrDefault(int key, int defaultValue) {
+				if (key == 0) {
+					return zeroValuePutted ? zeroValue : defaultValue;
+				}
+
+				for (int k = key;; k++) {
+					int index = indexOf(k);
+					if (keys[index] == 0) {
+						return defaultValue;
+					}
+
+					if (keys[index] == key) {
+						return values[index];
+					}
+				}
+			}
+
+			public boolean contains(int key) {
+				if (key == 0) {
+					return zeroValuePutted;
+				}
+
+				for (int k = key;; k++) {
+					int index = indexOf(k);
+					if (keys[index] == 0) {
+						return false;
+					}
+
+					if (keys[index] == key) {
+						return true;
+					}
+				}
+			}
+
+			private int indexOf(int key) {
+				return key & mask;
+			}
 		}
 		
 	}
--- src/main/java/ape/chang/leetcode/P678.java
+++ src/main/java/ape/chang/leetcode/P678.java
@@ -3,6 +3,8 @@ package ape.chang.leetcode;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class P678 {
@@ -17,17 +19,38 @@ public class P678 {
 	
 	class Solution {
 	    public boolean checkValidString(String s) {
-	    	char[] a = s.toCharArray();
-	    	int n = a.length;
-	    	
-	    	char[] leftParentheses = new char[n];
-	    	char[] rightParentheses = new char[n];
-	    	int l, r;
-	    	for (int i = 0; i < n; ++i) {
-	    		if (a[i] == '(') {
-	    			
+	    	char[] b = new char[s.length()];
+	    	int q = -1;
+	    	for (char c : s.toCharArray()) {
+	    		if (c == ')' && q >= 0 && b[q] == '(') {
+	    			q--;
+	    		} else {
+	    			b[++q] = c;
 	    		}
 	    	}
+	    	
+	    	if (q == -1) {
+	    		return true;
+	    	} else {
+	    		int p = 0;
+	    		while (p < q && b[p] == '(' && b[q] == ')') {
+	    			p++;
+	    			q--;
+	    		}
+	    		
+	    		if (p == q) {
+	    			return b[p] == '*';
+	    		} 
+	    		
+	    		if (p > q){
+	    			return true;
+	    		}
+	    		
+	    		// p < q
+	    		b = Arrays.copyOfRange(b, p, q+1);
+	    		
+	    	}
+	    	
 	    	return true;
 	    }
 	}
