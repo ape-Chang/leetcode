d093fa4427164ff794d6cf6fb6ba63a1fb120ef8
ape.chang <ape.chang.1989@gmail.com>
2017-10-27 22:59:43 +0800

    add some tests

--- src/main/java/ape/chang/primitives/IntSet.java
+++ src/main/java/ape/chang/primitives/IntSet.java
@@ -1,5 +1,10 @@
 package ape.chang.primitives;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class IntSet {
 
 	// The API
@@ -103,5 +108,26 @@ public class IntSet {
 	int mask;
 
 	static final int NOT_A_KEY = Integer.MAX_VALUE;
+	
+	// The test
+	
+	@Test
+	public void test() {
+		IntSet set = new IntSet();
+		assertThat(set.isEmpty(), equalTo(true));
+		for (int i = 0; i < 32; ++i) {
+			set.add(i);
+			assertThat(set.isEmpty(), equalTo(false));
+			assertThat(set.size(), equalTo(i+1));
+			assertThat(set.contains(i), equalTo(true));
+		}
+		for (int i = 0; i < 32; ++i) {
+			set.add(i);
+			assertThat(set.size(), equalTo(32));
+		}
+		for (int i = 0; i < 32; ++i) {
+			set.remove(i);
+		}
+	}
 
 }
--- src/main/java/ape/chang/primitives/IntStack.java
+++ src/main/java/ape/chang/primitives/IntStack.java
@@ -1,5 +1,10 @@
 package ape.chang.primitives;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class IntStack {
 	
 	/*
@@ -32,6 +37,10 @@ public class IntStack {
 		return size == 0;
 	}
 	
+	public int size() {
+		return size;
+	}
+	
 	// The implementation
 	
 	int[] stack = new int[16];
@@ -44,4 +53,23 @@ public class IntStack {
 		stack = newStack;
 	}
 	
+	//
+	
+	@Test
+	public void test() {
+		IntStack stack = new IntStack();
+		assertThat(stack.isEmpty(), equalTo(true));
+		assertThat(stack.size(), equalTo(0));
+		stack.push(1);
+		assertThat(stack.isEmpty(), equalTo(false));
+		assertThat(stack.top(), equalTo(1));
+		for (int i = 0; i < 16; ++i) {
+			stack.push(i);
+		}
+		assertThat(stack.top(), equalTo(15));
+		for (int i = 0; i < 16; ++i) {			
+			assertThat(stack.pop(), equalTo(15-i));
+		}
+		assertThat(stack.size(), equalTo(1));
+	}
 }
