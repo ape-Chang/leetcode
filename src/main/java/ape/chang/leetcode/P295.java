be4a6da91bb6e775ffb8a983263a3e1336904bb2
ape-Chang <ape.chang.1989@gmail.com>
2017-10-29 11:56:14 +0800

    hands on 297/295

---
+++ src/main/java/ape/chang/leetcode/P295.java
@@ -0,0 +1,21 @@
+package ape.chang.leetcode;
+
+public class P295 {
+	
+	class MedianFinder {
+
+	    /** initialize your data structure here. */
+	    public MedianFinder() {
+	        
+	    }
+	    
+	    public void addNum(int num) {
+	        
+	    }
+	    
+	    public double findMedian() {
+	        return .0f;
+	    }
+	}
+
+}
---
+++ src/main/java/ape/chang/leetcode/P297.java
@@ -0,0 +1,18 @@
+package ape.chang.leetcode;
+
+public class P297 {
+	
+	public class Codec {
+
+	    // Encodes a tree to a single string.
+	    public String serialize(TreeNode root) {
+	        return null;
+	    }
+
+	    // Decodes your encoded data to tree.
+	    public TreeNode deserialize(String data) {
+	    	return null;
+	    }
+	}
+
+}
