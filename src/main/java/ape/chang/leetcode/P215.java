c2b3255fb430e530e165f3e5735b798c1ee093f2
ape.chang <ape.chang.1989@gmail.com>
2017-10-31 19:16:56 +0800

    fix 215

--- src/main/java/ape/chang/leetcode/P215.java
+++ src/main/java/ape/chang/leetcode/P215.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 
 import java.util.Arrays;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class P215 {
@@ -14,31 +15,54 @@ public class P215 {
 	@Test
 	public void test() {
 		assertThat(solution.findKthLargest(new int[] {3,2,1,5,6,4}, 2), equalTo(5));
+		assertThat(solution.findKthLargest(new int[] {2,1}, 2), equalTo(1));
+	}
+	
+	@Ignore
+	@Test
+	public void testPartition() {
+		assertThat(solution.partition(new int[] {3,2,1,5,6,4}), equalTo(2));
+		assertThat(solution.partition(new int[] {2, 1}), equalTo(1));
+		assertThat(solution.partition(new int[] {1, 2}), equalTo(0));
+		assertThat(solution.partition(new int[] {2, 2, 3}), equalTo(0));
+		assertThat(solution.partition(new int[] {2, 2, 1, 3}), equalTo(1));
 	}
 	
 	class Solution {
 	    public int findKthLargest(int[] nums, int k) {
-	    	int n = nums.length;
-	        return p(Arrays.copyOf(nums, n), 0, n - 1, k);
-	    }
-	    
-	    private int p(int[] a, int l, int h, int k) {
-	    	int p = a[l], i = l, j = h, t;
-	    	
-	    	for (++i; i <= j && a[i] >= p; ++i)
-	    		;
-	    	
-	    	while (i < j) {
-	    		if (a[j] >= p) {
-	    			t    = a[j];
-	    			a[j] = a[i];
-	    			a[i] = t;
-	    			++i;
+	    	int n = nums.length, l = 0, h = n-1;
+	    	int[] a = Arrays.copyOf(nums, n);
+	    	while (true) {
+	    		int p = partition(a, l, h);
+	    		if (k-1 == h-p) {
+	    			return a[p];
+	    		}
+	    		
+	    		if (h-p >= k) {
+	    			l = p+1;
 	    		} else {
-	    			--j;
+	    			k = k - (h-p+1);
+	    			h = p-1;
 	    		}
 	    	}
-	    	return 0;
+	    }
+	    
+	    private int partition(int[] a, int l, int h) {
+	    	int p = a[l];
+	    	while (l < h) {	    		
+	    		for (; l < h && a[h] >= p; --h)
+	    			;
+	    		a[l] = a[h];
+	    		for (; l < h && a[l] <  p; ++l)
+	    			;
+	    		a[h] = a[l];
+	    	}
+	    	a[l] = p;
+	    	return l;
+	    }
+	    
+	    private int partition(int[] a) {
+	    	return partition(a, 0, a.length-1);
 	    }
 	}
 
