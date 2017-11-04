d55de2da7530d86503a0295d2f8ab05b3ac6bdf8
ape-Chang <ape.chang.1989@gmail.com>
2017-11-04 20:35:58 +0800

    nothing more to do with 698

--- src/main/java/ape/chang/leetcode/P698.java
+++ src/main/java/ape/chang/leetcode/P698.java
@@ -3,6 +3,8 @@ package ape.chang.leetcode;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class P698 {
@@ -11,22 +13,51 @@ public class P698 {
 	
 	@Test
 	public void test() {
-		assertThat(solution.canPartitionKSubsets(new int[] {4,3,2,3,5,2,1}, 4), equalTo(true));
-		assertThat(solution.canPartitionKSubsets(new int[] {2,2,2,2,3,4,5}, 4), equalTo(false));
+//		assertThat(solution.canPartitionKSubsets(new int[] {4,3,2,3,5,2,1}, 4), equalTo(true));
+//		assertThat(solution.canPartitionKSubsets(new int[] {2,2,2,2,3,4,5}, 4), equalTo(false));
+//		assertThat(solution.canPartitionKSubsets(new int[] {3522,181,521,515,304,123,2512,312,922,407,146,1932,4037,2646,3871,269}, 5), equalTo(true));
+		assertThat(solution.canPartitionKSubsets(new int[] {730,580,401,659,5524,405,1601,3,383,4391,4485,1024,1175,1100,2299,3908}, 4), equalTo(true));
 	}
 	
 	class Solution {
 	    public boolean canPartitionKSubsets(int[] a, int k) {
-	    	int s = 0, m = 0;
-	    	for (int i : a) {
-	    		s += i;
-	    		m = Math.max(m, i);
+	    	Arrays.sort(a);
+	    	
+	    	int s = 0, n = a.length;
+	    	for (int i = 0; i < n; ++i) {
+	    		s += a[i];
 	    	}
+	    	
 	    	if (s % k != 0) {
 	    		return false;
 	    	}
-	    	if (m > s/k) {
-	    		return false;
+	    	
+	    	int[] b = new int[k];
+	    	Arrays.fill(b, s/k);
+	    	
+	    	for (int i = a.length-1; i >= 0; --i, Arrays.sort(b)) {
+	    		int j;
+	    		for (j = 0; j < k; ++j) {
+	    			if (b[j] == a[i]) {
+	    				break;
+	    			}
+	    		}
+	    		if (j != k) {
+	    			b[j] -= a[i];
+	    			continue;
+	    		} else {
+	    			for (j = k-1; j >= 0; --j) {
+	    				if (b[j] > a[i]) {
+	    					break;
+	    				}
+	    			}
+	    			if (j < 0) {
+	    				return false;
+	    			} else {
+	    				b[j] -= a[i];
+	    			}
+	    		}
+	    		
 	    	}
 	        return true;
 	    }
