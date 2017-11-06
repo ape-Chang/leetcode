bb0b3202cac36a816ca1f2a141d6f81ea7e8e4e1
ape-Chang <ape.chang.1989@gmail.com>
2017-11-07 00:15:55 +0800

    another 2

---
+++ src/main/java/ape/chang/leetcode/P394.java
@@ -0,0 +1,11 @@
+package ape.chang.leetcode;
+
+public class P394 {
+	
+	class Solution {
+	    public String decodeString(String s) {
+	        
+	    }
+	}
+
+}
---
+++ src/main/java/ape/chang/leetcode/P474.java
@@ -0,0 +1,11 @@
+package ape.chang.leetcode;
+
+public class P474 {
+	
+	class Solution {
+	    public int findMaxForm(String[] strs, int m, int n) {
+	        
+	    }
+	}
+
+}
