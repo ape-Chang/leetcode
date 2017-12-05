358184a4adc22a0071da2c11e6c24f4270ea96cd
ape-Chang <ape.chang.1989@gmail.com>
2017-12-06 00:50:58 +0800

    fix 725

--- src/main/java/ape/chang/leetcode/P725.java
+++ src/main/java/ape/chang/leetcode/P725.java
@@ -4,7 +4,37 @@ public class P725 {
 	
 	class Solution {
 	    public ListNode[] splitListToParts(ListNode root, int k) {
-	        
+	        int n = length(root);
+	        int d = n / k;
+	        int r = n % k;
+	        ListNode[] heads = new ListNode[k];
+	        for (int i = 0; i < k; ++i) {
+	        	if (d == 0 && r == 0) {
+	        		heads[i] = null;
+	        	} else {
+	        		ListNode h = new ListNode(0), p = h;
+	        		for (int j = 0; j < d; ++j) {
+	        			p.next = root;
+	        			root = root.next;
+	        			p = p.next;
+	        		}
+	        		if (r-- > 0) {
+	        			p.next = root;
+	        			root = root.next;
+	        			p = p.next;
+	        		}
+	        		p.next = null;
+	        		heads[i] = h.next;
+	        	}
+	        }
+	        return heads;
+	    }
+	    
+	    int length(ListNode head) {
+	    	int n = 0;
+	    	for (; head != null; ++n, head = head.next) 
+	    		;
+	    	return n;
 	    }
 	}
 
