dacebdf93d0ecacfa4131039f9751d075792ddd3
ape-Chang <ape.chang.1989@gmail.com>
2017-12-23 22:56:39 +0800

    still can't fix 220

--- src/main/java/ape/chang/leetcode/P220.java
+++ src/main/java/ape/chang/leetcode/P220.java
@@ -1,11 +1,23 @@
 package ape.chang.leetcode;
 
 public class P220 {
-	
+
 	class Solution {
-	    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-	        
-	    }
+		public boolean containsNearbyAlmostDuplicate(int[] a, int k, int t) {
+			if (k <= 0 || t < 0) {
+				return false;
+			}
+
+			for (int i = 0; i < a.length; ++i) {
+				for (int j = i + 1, n = Math.min(i + k, a.length - 1); j <= n; ++j) {
+					// a[i] - a[j] may overflow, however, i-j never overflow
+					if (Math.abs(((long) a[j]) - a[i]) <= ((long) t)) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
 	}
 
 }
